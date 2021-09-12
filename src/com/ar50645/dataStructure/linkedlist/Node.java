bb6864bf3e008b63e0c0006377be2e4fbc40c64b
Abhishek R <abhranja@gmail.com>
2021-09-12 15:30:05 -0700

    update LL Node

--- src/com/ar50645/dataStructure/linkedlist/Node.java
+++ src/com/ar50645/dataStructure/linkedlist/Node.java
@@ -9,6 +9,11 @@ public class Node {
         this.next = next;
     }
 
+    public Node(int data) {
+        this.data = data;
+        this.next = null;
+    }
+
     public int getData() {
         return data;
     }
