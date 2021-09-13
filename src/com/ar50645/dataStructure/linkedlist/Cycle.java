0cd31a642e672ef62376d9125481a82fa688f5dd
Abhishek R <abhranja@gmail.com>
2021-09-13 13:25:17 -0700

    find cycle length

--- src/com/ar50645/dataStructure/linkedlist/Cycle.java
+++ src/com/ar50645/dataStructure/linkedlist/Cycle.java
@@ -22,4 +22,35 @@ public class Cycle {
         return false;
     }
 
+    //    Given a linked list that has a cycle, find the length of the cycle. The length is in number of nodes
+    public static int findCycleLength(Node head){
+        Node fast = head, slow = head;
+        while (fast != null) {
+            fast = fast.getNext();
+            if(fast == slow){
+                break;
+            }
+            if(fast != null){
+                fast = fast.getNext();
+                if(fast == slow){
+                    break;
+                }
+            }
+            slow = slow.getNext();
+        }
+
+        if(fast == null){  // no cycle found
+            return -1;
+        }
+
+        fast = fast.getNext();
+        int nodesPassed = 1;
+
+        while (fast != slow){
+            fast = fast.getNext();
+            nodesPassed++;
+        }
+        return nodesPassed;
+    }
+
 }
