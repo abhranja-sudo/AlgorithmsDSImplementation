06121dc5b7160e193b6ea85caa361f5fe73899be
Abhishek R <abhranja@gmail.com>
2021-09-25 13:57:42 -0700

    CoinChange

---
+++ src/com/ar50645/DP/CoinChange.java
@@ -0,0 +1,24 @@
+package com.ar50645.DP;
+
+/*
+You are given an integer array coins representing coins of different denominations and an integer amount representing a total amount of money.
+Return the number of combinations that make up that amount. If that amount of money cannot be made up by any combination of the coins, return 0.
+You may assume that you have an infinite number of each kind of coin.
+ */
+public class CoinChange {
+    public int change(int amount, int[] coins) {
+        int[] memo = new int[amount + 1];
+        memo[0] = 1;
+        return change(amount, coins, memo);
+    }
+    public int change(int amount, int[] coins, int[] memo) {
+
+        for (int coin: coins)
+            for(int i = 1; i <= amount; i++) {
+                if(i - coin >= 0)
+                    memo[i] = memo[i] + memo[i - coin];
+            }
+        return memo[memo.length - 1];
+
+    }
+}
