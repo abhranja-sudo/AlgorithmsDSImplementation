40d1eb66d62df66b3a2e0a531f429b9c5fc08697
Abhishek R <abhranja@gmail.com>
2021-11-05 16:43:01 -0700

    Tree | Diameter

--- src/com/ar50645/dataStructure/binaryTree/BottomTop.java
+++ src/com/ar50645/dataStructure/binaryTree/BottomTop.java
@@ -33,4 +33,42 @@ public class BottomTop {
 
         return 1 + Math.max(leftHeight, rightHeight);
     }
+
+
+    // Find the Diameter of a Binary Tree. The Diameter is the longest path from any 2 nodes in the tree.
+    public static int diameter(Node root) {
+        return diameterHelper(root).longestPath;
+    }
+
+    public static Result diameterHelper(Node root) {
+        if(root == null) {
+            return new Result(-1, 0);
+        }
+
+        Result leftResult = diameterHelper(root.left);
+        if(leftResult == null) {
+            leftResult = new Result(0, 0);
+        }
+
+        Result rightResult = diameterHelper(root.right);
+        if(rightResult == null) {
+            rightResult = new Result(0, 0);
+        }
+
+        int height = 1 + Math.max(leftResult.height, rightResult.height);
+        int longestPath = Math.max(Math.max(leftResult.longestPath, rightResult.longestPath), leftResult.height + rightResult.height + 1);
+        return new Result(height, longestPath);
+
+    }
+
+    public static class Result {
+        int height;
+        int longestPath;
+
+        public Result(int height, int longestPath) {
+            this.height = height;
+            this.longestPath = longestPath;
+        }
+    }
+
 }
