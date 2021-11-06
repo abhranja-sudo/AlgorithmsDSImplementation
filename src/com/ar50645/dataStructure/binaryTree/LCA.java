a45786650a15748fc03f3cbf34cc6f997c937d1d
Abhishek R <abhranja@gmail.com>
2021-11-06 16:08:46 -0700

    Tree | LCA - parent given

---
+++ src/com/ar50645/dataStructure/binaryTree/LCA.java
@@ -0,0 +1,100 @@
+package com.ar50645.dataStructure.binaryTree;
+
+public class LCA {
+
+    /**
+     * Given 2 nodes of a tree, find their lowest common ancestor (LCA). Assume that each node has a parent pointer.
+     */
+
+    public static Node findLCA(Node a , Node b) {
+        if(a == null || b == null) {
+            return null;
+        }
+        Node aPointer = a, bPointer = b;
+
+        //get aDepth
+        int aDepth = -1;
+        while (aPointer != null) {
+            aDepth++;
+            aPointer = aPointer.getParent();
+        }
+
+        //get bDepth
+        int bDepth = -1;
+        while (bPointer != null) {
+            bDepth++;
+            bPointer = bPointer.getParent();
+        }
+
+        //Raise the lower node
+        Node x = aDepth > bDepth ? a : b;
+        for(int i = 0; i < Math.abs(aDepth - bDepth); i++) {
+            x = x.getParent();
+        }
+
+        //Raise both node until they meet at LCA
+        Node y = aDepth > bDepth ? b: a;
+        while (x != y) {
+            x = x.getParent();
+            y = y.getParent();
+        }
+
+        return x;
+    }
+
+
+    private class Node {
+        Node left;
+        Node right;
+        Node parent;
+        int value;
+        boolean isVisited;
+
+        public Node(Node left, Node right, int value, boolean isVisited) {
+            this.left = left;
+            this.right = right;
+            this.value = value;
+            this.isVisited = isVisited;
+        }
+
+        public Node getLeft() {
+            return left;
+        }
+
+        public void setLeft(Node left) {
+            this.left = left;
+        }
+
+        public Node getRight() {
+            return right;
+        }
+
+        public void setRight(Node right) {
+            this.right = right;
+        }
+
+        public Node getParent() {
+            return parent;
+        }
+
+        public void setParent(Node parent) {
+            this.parent = parent;
+        }
+
+        public int getValue() {
+            return value;
+        }
+
+        public void setValue(int value) {
+            this.value = value;
+        }
+
+        public boolean isVisited() {
+            return isVisited;
+        }
+
+        public void setVisited(boolean visited) {
+            isVisited = visited;
+        }
+    }
+}
