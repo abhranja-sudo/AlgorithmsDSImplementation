f2dae2434805916dd1d2385873e85e595e881860
Abhishek R <abhranja@gmail.com>
2021-11-14 14:26:39 -0800

    BST | Successor

---
+++ src/com/ar50645/dataStructure/binarySearchTree/Successor.java
@@ -0,0 +1,45 @@
+package com.ar50645.dataStructure.binarySearchTree;
+
+import com.ar50645.dataStructure.binaryTree.Node;
+
+public class Successor {
+
+    /**
+     *   Given a Node N in a BST, find the node with the next largest value, also known as the successor of the node.
+     */
+
+    public static Node findSuccessor(Node root, Node n) {
+        if(root == null)
+            return null;
+
+        if(root.getRight() != null)
+            return getLeftMostNode(n);
+
+        return getFirstRightParentNode(root, n);
+    }
+
+    private static Node getFirstRightParentNode(Node root, Node n) {
+        Node current = root;
+        Node result = null;
+
+        while (current != null) {
+            if(n.getValue() < current.getValue()) {
+                result = current;
+                current = current.getLeft();
+            }
+            else if(n.getValue() > current.getValue())
+                current = current.getRight();
+            else
+                break;
+        }
+        return result;
+    }
+
+    private static Node getLeftMostNode(Node node) {
+        Node current = node;
+        while (current.getLeft() != null) {
+            current = current.getLeft();
+        }
+        return current;
+    }
+}
