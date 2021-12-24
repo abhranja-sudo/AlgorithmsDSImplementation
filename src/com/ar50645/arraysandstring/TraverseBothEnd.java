e1455dd57d5d0d076a1af6bf390949a6cf12a864
Abhishek R <abhranja@gmail.com>
2021-12-23 17:29:09 -0800

    TwoSum

--- src/com/ar50645/arraysandstring/TraverseBothEnd.java
+++ src/com/ar50645/arraysandstring/TraverseBothEnd.java
@@ -1,5 +1,7 @@
 package com.ar50645.arraysandstring;
 
+import com.ar50645.common.Pair;
+
 public class TraverseBothEnd {
     /**
      * Reverse the order of elements in an array. For example, A = [1,2,3,4,5,6], Output = [6,5,4,3,2,1]
@@ -25,4 +27,27 @@ public class TraverseBothEnd {
         a[i] = a[j];
         a[j] = temp;
     }
+
+
+//    2 Sum Problem: Given a sorted array of integers, find two numbers in the array that sum to a given integer target.
+    public Pair<Integer> twoSum(int[] a, int target) {
+
+        if(a == null)
+            return null;
+
+        int start = 0;
+        int end = a.length - 1;
+
+        while (start < end) {
+            int sum = a[start] + a[end];
+            if(sum == target)
+                return new Pair<>(a[start], a[end]);
+            else if(sum < target)
+                start++;
+            else
+                end--;
+        }
+        return null;
+    }
+
 }
