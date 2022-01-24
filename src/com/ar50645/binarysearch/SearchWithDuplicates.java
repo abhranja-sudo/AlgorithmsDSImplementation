d0d44adb02adb1fc79693d59bfe53c6a3cd02ffd
Abhishek R <abhranja@gmail.com>
2022-01-24 13:26:13 -0800

    InsertionOrder

--- src/com/ar50645/binarysearch/SearchWithDuplicates.java
+++ src/com/ar50645/binarysearch/SearchWithDuplicates.java
@@ -46,4 +46,27 @@ public class SearchWithDuplicates {
     }
 
 
-}
+    //find insertion index
+    public static int findInsertionIndex(int[] a, int target) {
+        if (a == null)
+            return -1;
+        if (a.length == 0)
+            return 0;
+        int low = 0;
+        int high = a.length - 1;
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            if (a[mid] <= target) {
+                if (mid == a.length - 1)
+                    return a.length;
+                low = mid + 1;
+            } else {
+                if (mid == 0 || a[mid - 1] <= target)
+                    return mid;
+                high = mid - 1;
+            }
+        }
+        return -1; // should not happen, you can also throw an exception here}
+
+    }
+    }
