9d681edf6ce525442cf06e4ca83011479fa7018c
Abhishek R <abhranja@gmail.com>
2022-01-26 12:06:52 -0800

    GenerateParenthesis

---
+++ src/com/ar50645/recursion/GenerateParenthesis.java
@@ -0,0 +1,27 @@
+package com.ar50645.recursion;
+
+public class GenerateParenthesis {
+    public List<String> generateParenthesis(int n) {
+        List<String> ans = new ArrayList();
+        backtrack(ans, new StringBuilder(), 0, 0, n);
+        return ans;
+    }
+
+    public void backtrack(List<String> ans, StringBuilder buffer, int open, int close, int max){
+        if (buffer.length() == max * 2) {
+            ans.add(buffer.toString());
+            return;
+        }
+
+        if (open < max) {
+            buffer.append("(");
+            backtrack(ans, buffer, open + 1, close, max);
+            buffer.deleteCharAt(buffer.length() - 1);
+        }
+        if (close < open) {
+            buffer.append(")");
+            backtrack(ans, buffer, open, close + 1, max);
+            buffer.deleteCharAt(buffer.length() - 1);
+        }
+    }
+}
